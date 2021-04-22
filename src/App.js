61ef2d7171e97bf2f22c70f596a2a4d96538c2b0
chenjianrui <janine.jian.chen@gmail.com>
2021-04-22 09:36:26 +0800

    新增hooks简单例子

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component} from "react";
 import "./App.css";
+import ClickTime from "./ClickTime/ClickTime";
 
 class App extends Component{
   render(){
     return(
       <div className="App">
         <h1> Hello, World! </h1>
+        <ClickTime></ClickTime>
       </div>
     );
   }
